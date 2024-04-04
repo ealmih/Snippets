ce07153207e71130854532dbc105ddc5abbe8367
ealmih <wxnik1rmuwfa@mail.ru>
2024-04-04 16:19:19 +0300

    формы в джанго

--- MainApp/views.py
+++ MainApp/views.py
@@ -10,20 +10,26 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    form = SnippetForm()
-    context = {
-        'pagename': 'Добавление нового сниппета',
-        'form':form
-        }
-    return render(request, 'pages/add_snippet.html', context)
-
+    if request.method == 'GET':
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form':form
+            }
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
+        return render(request, 'pages/add_snippet.html', {'form': form})
 
 def snippets_page(request):
     snippets = Snippet.objects.all()
     context = {
         'pagename': 'Просмотр сниппетов',
         'snippets': snippets
-        }
+    }
     return render(request, 'pages/view_snippets.html', context)
 
 def snippet_detail(request, snippet_id):
@@ -34,14 +40,14 @@ def snippet_detail(request, snippet_id):
         }
     return render(request, 'pages/snippet_detail.html', context)
 
-def snippet_create(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        lang = request.POST['lang']
-        code = request.POST['code']
-        snippet = Snippet(name=name, lang=lang, code=code)
-        snippet.save()
-        return redirect('snippets-list')
+# def snippet_create(request):
+#     if request.method == 'POST':
+#         name = request.POST['name']
+#         lang = request.POST['lang']
+#         code = request.POST['code']
+#         snippet = Snippet(name=name, lang=lang, code=code)
+#         snippet.save()
+#         return redirect('snippets-list')
     
 def create_snippet(request):
    form = SnippetForm()
