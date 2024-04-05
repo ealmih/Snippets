867fdaae42ecfc2308009a0a41fa60544b3c9441
ealmih <wxnik1rmuwfa@mail.ru>
2024-04-05 16:36:20 +0300

    settings hosts

--- MainApp/views.py
+++ MainApp/views.py
@@ -41,7 +41,8 @@ def snippets_page(request):
     snippets = Snippet.objects.filter(public=True)
     context = {
         'pagename': 'Просмотр сниппетов',
-        'snippets': snippets
+        'snippets': snippets,
+        'count': Snippet.objects.filter().count()
     }
     return render(request, 'pages/view_snippets.html', context)
 
