1c2f6291bb2a91d0379a77915ea89b941b0632ad
ealmih <wxnik1rmuwfa@mail.ru>
2024-04-05 12:15:26 +0300

    error fo login

--- MainApp/views.py
+++ MainApp/views.py
@@ -100,7 +100,11 @@ def login(request):
         if user is not None:
             auth.login(request, user)
         else:
-            pass
+            context = {
+            'pagename': 'PythonBin',
+            'errors': ['wrong username or password']
+            }
+            return render(request, 'pages/index.html', context)
     return redirect('home')
 
 def logout(request):
